51a84e73b08ce4889b3bcef8ad6f716a2f667d06
Redi.Kustiwa <redi.kustiwa@xapiens.id>
2025-07-22 17:18:59 +0700

    Change Password

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@ $base_url = $protocol . $_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['S
  
 define('BASE_URL', str_replace("public/", "", $base_url));
 define('DB_HOST', 'localhost');
-define('DB_USER', 'admin');
-define('DB_PASS', 'admins');
+define('DB_USER', 'root');
+define('DB_PASS', 'Kendari@2025');
 define('DB_NAME', 'siapel');
